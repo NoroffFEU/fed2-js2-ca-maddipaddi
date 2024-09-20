3338c62c290cf35a6dbedd1071e2a7aca607c7ef
Maddie <madelen.sletteberg@gmail.com>
2024-09-20 13:22:19 +0200

    feat: add API login function

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,22 @@
-export async function login({ email, password }) {}
+import { authFetch } from "../authFetch";
+import { API_AUTH_LOGIN } from "../constants";
+import { save } from "../storage/save";
+
+export async function login({ email, password }) {
+    const response = await authFetch(API_AUTH_LOGIN, {
+        method: "POST",
+        body: JSON.stringify({email, password})
+        });
+      
+        if (response.ok) {
+            const { accessToken, ...profile } = (await response.json()).data; 
+
+            save("token", accessToken);
+            save("profile", profile);
+            return profile;
+        }
+      
+       
+        throw new Error("You were not able to log in.")
+}
+ 
