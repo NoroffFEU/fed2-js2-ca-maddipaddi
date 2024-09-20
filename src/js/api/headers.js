45c4c55951291dc434527f2284aa22c30ee89310
Maddie <madelen.sletteberg@gmail.com>
2024-09-20 13:20:21 +0200

    feat: add Headers for authentication

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,11 +1,15 @@
 import { API_KEY } from "./constants";
 
-export function headers() {
+export function headers(hasBody = false) {
   const headers = new Headers();
 
   if (API_KEY) {
     headers.append("X-Noroff-API-Key", API_KEY);
   }
 
+  if (hasBody) {
+    headers.append("Content-Type", "application/json")
+  }
+  
   return headers;
 }
