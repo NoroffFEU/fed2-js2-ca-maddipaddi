35a480b5791ebdc35e528b14ccb1c0034594a341
Maddie <madelen.sletteberg@gmail.com>
2024-09-20 13:22:43 +0200

    feat: add UI login handler to process form data

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,10 @@
-export async function onLogin(event) {}
+import { login } from "../../api/auth/login";
+
+export async function onLogin(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const account = Object.fromEntries(formData.entries());
+
+   login(account); 
+}
