b7a34edfb44aae6a498817ea37df4cbc76faa02d
Maddie <madelen.sletteberg@gmail.com>
2024-09-20 13:23:14 +0200

    feat: add API register function

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,3 +1,6 @@
+import { API_AUTH_REGISTER } from "../constants";
+import { authFetch } from "../authFetch";
+
 export async function register({
   name,
   email,
@@ -5,4 +8,16 @@ export async function register({
   bio,
   banner,
   avatar,
-}) {}
+}) {
+
+  const response = await authFetch(API_AUTH_REGISTER, {
+  method: "POST",
+  body: JSON.stringify({name, email, password, bio, banner, avatar})
+  });
+
+  if (response.ok) {
+    return await response.json();
+  }
+
+  throw new Error("The account could not be registered.")
+}
