e776d90caa6538435d28df3459e0eec10e933c06
maddipaddi <madelen.sletteberg@gmail.com>
2024-09-23 16:27:17 +0200

    feat: improved error handling in login function

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,22 +1,27 @@
+import { displayMessage } from "../../ui/global/displayMessage";
+import { handleErrors } from "../../utilities/handleErrors";
 import { authFetch } from "../authFetch";
 import { API_AUTH_LOGIN } from "../constants";
 import { save } from "../storage/save";
 
 export async function login({ email, password }) {
-    const response = await authFetch(API_AUTH_LOGIN, {
-        method: "POST",
-        body: JSON.stringify({email, password})
-        });
-      
-        if (response.ok) {
-            const { accessToken, ...profile } = (await response.json()).data; 
+    try {
+        const response = await authFetch(API_AUTH_LOGIN, {
+            method: "POST",
+            body: JSON.stringify({email, password})
+            });
 
-            save("token", accessToken);
-            save("profile", profile);
-            return profile;
-        }
-      
-       
-        throw new Error("You were not able to log in.")
+            if (response.ok) {
+                const { accessToken, ...profile } = (await response.json()).data; 
+                save("token", accessToken);
+                save("profile", profile);
+                return profile;
+            } else {
+                handleErrors(response);
+            }
+          
+    } catch (error) {
+        displayMessage(error.message, "error");
+    }
 }
  
