0192759ef6d2dbc3b6d67aba84947bcf8a1ef3a1
maddipaddi <madelen.sletteberg@gmail.com>
2024-09-24 21:39:59 +0200

    feat: added function to create a post and post to the API

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,17 @@
-export async function createPost({ title, body, tags, media }) {}
+import { displayMessage } from "../../ui/global/displayMessage";
+import { authFetch } from "../authFetch";
+import { API_SOCIAL_POSTS } from "../constants";
+
+export async function createPost({ title, body, tags, media }) {
+    const response = await authFetch(API_SOCIAL_POSTS, {
+        method: "POST",
+        body: JSON.stringify({ title, body, tags, media })
+    })
+
+    if (response.ok) {
+        return displayMessage("Post has been created successfully!", "success");
+    }
+
+    throw new Error("Could not create post");
+    
+}
