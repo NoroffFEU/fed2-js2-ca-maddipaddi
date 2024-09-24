96383073b5514ef09c8379122ddfbf9c3d191db2
maddipaddi <madelen.sletteberg@gmail.com>
2024-09-24 21:38:31 +0200

    feat: added read posts function to fetch all posts, needs further refinement

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -1,5 +1,16 @@
+import { authFetch } from "../authFetch";
+import { API_SOCIAL_POSTS } from "../constants";
+
 export async function readPost(id) {}
 
-export async function readPosts(limit = 12, page = 1, tag) {}
+export async function readPosts(limit = 12, page = 1, tag) {
+    const response = await authFetch(API_SOCIAL_POSTS, {
+        
+    });
+
+    const posts = await response.json();
+    return posts.data;
+ 
+}
 
 export async function readPostsByUser(username, limit = 12, page = 1, tag) {}
