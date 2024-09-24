1a10dc6ab0384c10ac0a65701c48e8d55dbbaa1c
maddipaddi <madelen.sletteberg@gmail.com>
2024-09-24 21:41:13 +0200

    feat: added function to pull values from UI to pass in create post function

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,1 +1,28 @@
-export async function onCreatePost(event) {}
+import { createPost } from "../../api/post/create";
+import { setMediaObject } from "../../utilities/setMediaObject";
+import { stringToArray } from "../../utilities/stringToArray";
+import { displayMessage } from "../global/displayMessage";
+
+export async function onCreatePost(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const formValues = Object.fromEntries(formData.entries());
+
+    const tagsAsArray = stringToArray(formValues.tags);
+    const mediaAsObject = setMediaObject(formValues.mediaURL, formValues.mediaALT);
+   
+    const postData = {
+       title: formValues.title,
+       media: mediaAsObject,
+       body: formValues.body,
+       tags: tagsAsArray
+   }
+
+    try {
+        await createPost(postData); 
+    } catch (error) {
+        displayMessage(error.message, "error");
+    }
+}
+
