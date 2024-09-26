9bf2cb8d142fc63fff8c0cc928822dd1495ca154
maddipaddi <madelen.sletteberg@gmail.com>
2024-09-26 12:24:55 +0200

    feat: renders profile and posts by user on profile page

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,3 +1,7 @@
+import { renderPostsByUser } from "../../ui/post/read";
+import { profileTemplate } from "../../ui/templates/profileTemplate";
 import { authGuard } from "../../utilities/authGuard";
 
 authGuard();
+profileTemplate();
+renderPostsByUser();
