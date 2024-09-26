cc9303b8d5399c089a40d0ec48cac864138ae229
maddipaddi <madelen.sletteberg@gmail.com>
2024-09-26 12:33:27 +0200

    feat: sends data from form to update post function when sumbitted

--- src/js/ui/post/update.js
+++ src/js/ui/post/update.js
@@ -1,1 +1,25 @@
-export async function onUpdatePost(event) {}
+import { updatePost } from "../../api/post/update";
+import { findPostID } from "../../utilities/findPostID";
+import { setMediaObject } from "../../utilities/setMediaObject";
+import { stringToArray } from "../../utilities/stringToArray";
+
+export async function onUpdatePost(event) {
+    event.preventDefault();
+        const form = event.target;
+        const formData = new FormData(form);
+        const formValues = Object.fromEntries(formData.entries());
+  
+       const tagsAsArray = stringToArray(formValues.tags);
+       const mediaAsObject = setMediaObject(formValues.mediaURL, formValues.mediaALT);
+    
+       const id = findPostID();
+      
+       const postData = {
+          title: formValues.title,
+          media: mediaAsObject,
+          body: formValues.body,
+          tags: tagsAsArray
+      }
+  
+    updatePost(id, postData); 
+}
