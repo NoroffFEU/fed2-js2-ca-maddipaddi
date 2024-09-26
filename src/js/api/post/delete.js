84216cc7d6dd5a1e241f429bdd9e254c0a9d052f
maddipaddi <madelen.sletteberg@gmail.com>
2024-09-26 15:18:00 +0200

    feat: deletes post from API

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,1 +1,12 @@
-export async function deletePost(id) {}
+import { authFetch } from "../authFetch";
+import { API_SOCIAL_POSTS } from "../constants";
+
+export async function deletePost(id) {
+    if (!id) {
+        throw new Error("Delete requires a post id");
+    }
+
+    await authFetch(`${API_SOCIAL_POSTS}/${id}`, {
+        method: "DELETE"
+    })
+}
