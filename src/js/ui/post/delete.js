aaf1627de43b829653e3260b3090026fb2bd1421
maddipaddi <madelen.sletteberg@gmail.com>
2024-09-26 15:19:00 +0200

    feat: adds function that handles the event when delete button is clicked

--- src/js/ui/post/delete.js
+++ src/js/ui/post/delete.js
@@ -1,1 +1,30 @@
-export async function onDeletePost(event) {}
+import { deletePost } from "../../api/post/delete";
+
+
+export async function onDeletePost(event) {
+    event.preventDefault(); 
+    const redirectAfterDeletion = () => {
+        const checkForContent = () => {
+            const posts = document.querySelectorAll(".post-element");
+            return posts;
+        } 
+    
+        const result = checkForContent? window.location.href = "/" : window.location.reload(); 
+        return result;
+    }
+     
+    const deleteButton = event.target;
+    const postId = deleteButton.getAttribute("id");
+  
+   try {
+    await deletePost(postId);
+   
+  
+   } catch (error) {
+    throw new Error("The post could not be deleted.");
+   }
+   
+    alert("The post was deleted");
+    redirectAfterDeletion();
+}
+
