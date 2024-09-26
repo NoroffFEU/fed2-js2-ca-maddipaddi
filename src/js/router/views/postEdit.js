677d28f6f5d3b8bd4a6419ee7e3579a0a577b576
maddipaddi <madelen.sletteberg@gmail.com>
2024-09-26 12:34:34 +0200

    feat: populate the edit form when entering edit post page and adds formlistener on submit to update post

--- src/js/router/views/postEdit.js
+++ src/js/router/views/postEdit.js
@@ -1,3 +1,11 @@
+import { onUpdatePost } from "../../ui/post/update";
 import { authGuard } from "../../utilities/authGuard";
+import { populateForm } from "../../utilities/populatePostFormToEdit";
 
 authGuard();
+
+populateForm();
+
+const form = document.forms.editPost;
+
+form.addEventListener("submit", onUpdatePost);
