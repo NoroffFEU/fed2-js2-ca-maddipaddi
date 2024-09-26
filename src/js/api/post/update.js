450734752dd35628f03e031b3d8e2f7279661c67
maddipaddi <madelen.sletteberg@gmail.com>
2024-09-26 12:31:07 +0200

    feat: adds update function to update post through the API

--- src/js/api/post/update.js
+++ src/js/api/post/update.js
@@ -1,1 +1,22 @@
-export async function updatePost(id, { title, body, tags, media }) {}
+import { displayMessage } from "../../ui/global/displayMessage";
+import { authFetch } from "../authFetch";
+import { API_SOCIAL_POSTS } from "../constants";
+
+export async function updatePost(id, { title, body, tags, media }) {
+    if (!id) {
+        throw new Error("Update requires a post ID");
+    }
+
+    const response = await authFetch(`${API_SOCIAL_POSTS}/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, body, tags, media })
+    })
+
+    if (response.ok) {
+        displayMessage("Post updated successfully!", "success");
+    } else {
+        throw new Error("Failed to update the post");
+    }
+
+    return await response.json();
+}
