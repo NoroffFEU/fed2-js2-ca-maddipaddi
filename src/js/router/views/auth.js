fda9762c62355da7fe3aeb211b7f3a0baa043989
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2024-11-08 19:12:33 +0100

    chore: style auth page with login and register modals

--- src/js/router/views/auth.js
+++ src/js/router/views/auth.js
@@ -1,1 +1,16 @@
-alert("Auth Index Page");
+import { onRegister } from "../../ui/auth/register";
+import { onLogin } from "../../ui/auth/login";
+
+function registerInit() {
+  const form = document.forms.register;
+  form.addEventListener("submit", onRegister);
+}
+
+registerInit();
+
+function loginInit() {
+  const form = document.forms.login;
+  form.addEventListener("submit", onLogin);
+}
+
+loginInit();
