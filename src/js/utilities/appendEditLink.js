745c03551544d5f16316cfbbc17120403915fd26
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2024-11-09 15:00:39 +0100

    chore: style buttons

--- src/js/utilities/appendEditLink.js
+++ src/js/utilities/appendEditLink.js
@@ -22,10 +22,11 @@ export const appendEditLink = (post, author) => {
 
   if (author === userName) {
     const editLink = document.createElement("a");
+    editLink.className = "btn btn-light-theme m-1";
     editLink.innerText = "Edit";
     editLink.setAttribute("href", `/post/edit/?id=${post.id}`);
     editLink.setAttribute("id", "edit-link");
-    editLink.classList.add("button");
+
     return editLink;
   } else {
     return "";
